694a6927ed83a08dba02a598d82c71c51186578d
Brunetob <159277970+BrunetoSpy@users.noreply.github.com>
2024-02-08 21:01:34 -0500

    cmaddfuncitons
    prueba de paginación

--- film/app/details.tsx
+++ film/app/details.tsx
@@ -1,5 +1,96 @@
 import { Feather } from '@expo/vector-icons';
 import { Stack, useRouter, useLocalSearchParams } from 'expo-router';
+import React, { useEffect, useState } from "react";
+import { Button, Image, ScrollView, Text, YStack } from "tamagui";
+
+import { Container, Main, Subtitle, Title } from '../tamagui.config';
+import { BasicCards } from "~/components/BasicCard";
+import EditModal from "~/components/EditModal";
+import { fetchFilms } from "~/api/BasicPetitions";
+import axios from "axios";
+import { Pressable, View } from "react-native";
+
+export default function Details() {
+
+  const [header,setHeader] = useState("film")
+
+  const { name } = useLocalSearchParams();
+  const router = useRouter();
+  const [showEditModal, setShowEditModal]=useState(false);
+  const [isCreateModal, setIsCreateModal]=useState(false);
+  const [data, setData]=useState<Array<FetchResponses>>([]);
+
+
+  useEffect(() => {
+    setData([])
+    fetchFilms(header).then((response)=>setData(response))
+  }, []);
+
+  const changePage=(newPage:Array<FetchResponses>)=>{
+    setData(newPage)
+  }
+
+  const changeHeader=(newHeader:string)=>{
+    setHeader(newHeader)
+  }
+
+  const openEditModal=()=>{
+    setShowEditModal(true)
+  }
+
+  const closeEditModal=()=>{
+    setShowEditModal(false)
+    setIsCreateModal(false)
+  }
+
+  const BackButton = () => (
+    <Button
+      unstyled
+      flexDirection="row"
+      backgroundColor="black"
+      padding={8}
+      borderRadius={15}
+      pressStyle={{ opacity: 0.5 }}
+      onPress={router.back}
+      alignItems={'center'}
+      icon={<Feather name="chevron-left" size={16} color="white" />}>
+      <Text color="white">Volver</Text>
+    </Button>
+  );
+
+  return (
+    <Container style={{ backgroundColor: '#350A24' }}>
+      <Stack.Screen options={{ headerTitleStyle:{color: 'white'},headerStyle:{backgroundColor: '#752D59'}, title: '', headerLeft: () => <BackButton /> }} />
+
+      <Main>
+        <YStack>
+          <ScrollView>
+          <Text color="rgba(255,255,255,0.54)" fontWeight="bold" fontSize={40}>DASHBOARD</Text>
+          <Text color='#752D59' fontWeight="bold" fontSize={40}>{header.toUpperCase()}</Text>
+          <BasicCards data={data} setShowEditModal={openEditModal} pageChange={changePage} header={header} changeHeader={changeHeader}></BasicCards>
+          </ScrollView>
+        </YStack>
+        <View style={{position: "absolute",
+          bottom: 0,
+          right: 0}}>
+          <Pressable
+            style={{borderWidth: 4, borderRadius: 50, borderColor: 'white'}}
+            onPress={() => {
+              setIsCreateModal(true)
+              openEditModal();
+            }}>
+            <Image source={require('../assets/images/AddButton.png')} />
+          </Pressable>
+        </View>
+      </Main>
+      {showEditModal&&
+        <EditModal data={data} closeEditModal={closeEditModal} isCreate={isCreateModal} header={header}/>
+      }
+    </Container>
+  );
+}
+/*import { Feather } from '@expo/vector-icons';
+import { Stack, useRouter, useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import { Button, Text, YStack } from 'tamagui';
 
@@ -34,3 +125,4 @@ export default function Details() {
     </Container>
   );
 }
+*/
--- film/app/index.tsx
+++ film/app/index.tsx
@@ -1,4 +1,39 @@
 import { Stack, Link } from 'expo-router';
+import { Image, YStack } from "tamagui";
+
+import { Container, Main, Title, Subtitle, Button, ButtonText } from '../tamagui.config';
+
+export default function Page() {
+  return (
+    <Container style={{backgroundColor: '#350A24', textAlign: 'center', justifyContent:'center', fontStyle:'Inter' }}>
+      <Main justifyContent='center' alignItems='center'>
+        <Stack.Screen options={{ title: 'Films Projects'}} />
+        <Image
+          source={
+            require('../assets/images/cinecam.png')            
+          }
+          style={{
+            marginTop: 50,
+            marginBottom: 50,
+            width: 160,
+            height: 160,
+            
+          }}
+        />
+        <YStack>
+          <Title style={{ color: 'white', fontSize: 20, fontWeight: 'bold',  }}>Movie {'\n'} Making </Title>
+        </YStack>
+        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
+          <Button backgroundColor='#752D59' width='40%'>
+            <ButtonText>Begin</ButtonText>
+          </Button>
+        </Link>
+      </Main>
+    </Container>
+  );
+}
+
+/*import { Stack, Link } from 'expo-router';
 import { Image } from 'tamagui'
 import { YStack, Text, Input, Button, XStack } from 'tamagui';
 import { useRouter } from "expo-router";
@@ -21,7 +56,7 @@ export default function Page() {
           }}
       />
       <Main>
-        {/*<Stack.Screen options={{ title: 'MOVIE MAKING' }} />*/}
+        <Stack.Screen options={{ title: 'MOVIE MAKING' }} />
         <YStack>
           <Container margin={1} padding={5}>
             <Title color={"#ffffff"}>The Hunger</Title>
@@ -34,4 +69,4 @@ export default function Page() {
       </Main>
     </Container>
   );
-}
+}*/
